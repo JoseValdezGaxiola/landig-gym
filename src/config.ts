7fe073f7488191d1aa457a05ee03c5a66e0a66c9
Jose Valdez Gaxiola <josevaldezgaxiola@gmail.com>
2023-03-23 12:49:36 -0700

    fix: fix testomonial component responsiveness

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,24 @@ export const SITE = {
   description: "Best Solution For Coaches",
   image: "/profile.jpg",
 };
+
+export const TESTIMONIAL = {
+data: [
+  {
+    description: "The best coach services in the city, unique style of programming",
+    
+    image: "https://images.unsplash.com/photo-1639747280804-dd2d6b3d88ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    name: "Mario Lopez",
+  },
+  {
+    description: "Rapid learning trought excellent teaching method",
+    image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHBvcnRyYWl0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    name: "Joseph England",
+  },
+  {
+    description: "I was shock for getting results from week one,its worth the cost ",
+    image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    name: "Dylan Ambrose",
+  },
+]
+}
